41e50888539114bfc3fcab43759693b35456edfd
oalt <oa@lacheweg21.de>
2026-07-27 14:33:33 +0200

    Initial code commit

---
+++ src/MDD4All.Localization/Contracts/ILanguageSetter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MDD4All.Localization.Contracts
+{
+    public interface ILanguageSetter
+    {
+        event EventHandler? CultureChanged;
+
+        void SetCulture(string cultureName);
+
+        List<CultureInfo> SupportedCultures { get; set; }
+
+        CultureInfo CurrentCulture { get; }
+    }
+}
---
+++ src/MDD4All.Localization/LanguageSetter.cs
@@ -0,0 +1,57 @@
+﻿using MDD4All.Localization.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MDD4All.Localization
+{
+    public class LanguageSetter : ILanguageSetter
+    {
+        public LanguageSetter() 
+        {
+            SupportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("de-DE")
+            };
+
+            CurrentCulture = CultureInfo.CurrentUICulture;
+
+            SetCulture(CurrentCulture.Name);
+        }
+
+        public LanguageSetter(List<CultureInfo> supportedCutures, CultureInfo currentCulture)
+        {
+            SupportedCultures = supportedCutures;
+            CurrentCulture = currentCulture;
+
+            SetCulture(CurrentCulture.Name);
+        }
+
+        public event EventHandler? CultureChanged;
+
+        public List<CultureInfo> SupportedCultures { get; set; }
+    
+        public CultureInfo CurrentCulture { get; set; }
+
+        public void SetCulture(string cultureName)
+        {
+            CultureInfo? culture = null;
+            foreach (CultureInfo cultureInfo in SupportedCultures)
+            {
+                if (cultureInfo.Name == cultureName)
+                {
+                    culture = cultureInfo;
+                    break;
+                }
+            }
+
+            if (culture != null)
+            {
+                CurrentCulture = culture;
+
+                CultureChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
